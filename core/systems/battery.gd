ca955f03057e712bdf67962ba7cd3cb8bae30e30
William Edwards <shadowapex@gmail.com>
2022-11-30 23:14:18 -0800

    Add Battery class for fetching battery stats

---
+++ core/systems/battery.gd
@@ -0,0 +1,31 @@
+extends Object
+class_name Battery
+@icon("res://assets/icons/battery-charging.svg")
+
+const power_supply_dir = "/sys/class/power_supply"
+
+# Finds the battery path. If none is found, returns null.
+static func find_battery_path() -> Variant:
+	var power_dir: DirAccess = DirAccess.open(power_supply_dir)
+	var devices: PackedStringArray = power_dir.get_directories()
+	var battery_dir: String = ""
+	for folder in devices:
+		if folder.begins_with("BAT"):
+			battery_dir = folder
+			break
+	if battery_dir == "":
+		return null
+	print("Battery: Found battery path: ", battery_dir)
+	
+	return "/".join([power_supply_dir, battery_dir])
+
+
+# Returns the current battery capacity as a percentage
+static func get_capacity(battery: String) -> int:
+	var capacity_file: String = "/".join([battery, "capacity"])
+	var file: FileAccess = FileAccess.open(capacity_file, FileAccess.READ)
+	var bytes: PackedByteArray = file.get_buffer(100)
+	var str: String = bytes.get_string_from_ascii().strip_edges()
+	if not str.is_valid_int():
+		return -1
+	return str.to_int()
--- core/ui/menu/top_bar.gd
+++ core/ui/menu/top_bar.gd
@@ -1,16 +1,27 @@
 extends Control
 
+@onready var battery: String = Battery.find_battery_path()
 @onready var time_label: Label = $MarginContainer/HBoxContainer/TimeLabel
+@onready var battery_label: Label = $MarginContainer/HBoxContainer/BatteryLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var time_timer: Timer = Timer.new()
 	time_timer.timeout.connect(_on_update_time)
+	time_timer.timeout.connect(_on_update_battery)
 	time_timer.autostart = true
 	add_child(time_timer)
 	_on_update_time()
+	_on_update_battery()
 
 
+# Updates the battery capacity on timer timeout
+func _on_update_battery():
+	var current_capacity: int = Battery.get_capacity(battery)
+	battery_label.text = "{0}%".format([current_capacity])
+
+
+# Updates the current time on timer timeout
 func _on_update_time():
 	# year, month, day, weekday, hour, minute, second
 	var time = Time.get_datetime_dict_from_system()
