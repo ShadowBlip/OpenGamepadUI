c400e8f4db84e2ab869aa7d44a3adc555fa21e93
Derek J. Clark <derekjohn.clark@gmail.com>
2022-12-03 23:09:11 -0800

    QAM. Better InputMan. QAM Plugins

--- core/ui/menu/quick_access_menu.gd
+++ core/ui/menu/quick_access_menu.gd
@@ -1,6 +1,8 @@
 extends Control
 
 @onready var state_mgr: StateManager = get_node("/root/Main/StateManager")
+@onready var icon_bar: VBoxContainer = $MarginContainer/HBoxContainer/IconBar
+@onready var viewport: VBoxContainer = $MarginContainer/HBoxContainer/Viewport
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,3 +12,11 @@ func _ready() -> void:
 
 func _on_state_changed(from: int, to: int, _data: Dictionary) -> void:
 	visible = to == StateManager.State.QUICK_ACCESS_MENU
+
+	# Don't do anything if its not our time.
+	if not visible:
+		return
+
+	
+	var button: Button = icon_bar.get_child(0)
+	button.grab_focus()
