2b1d51e5da43fa3184ff0c226d47d4853e8618be
William Edwards <shadowapex@gmail.com>
2022-12-19 15:31:45 -0800

    Update xlib addon to godot4 beta9

--- addons/godot-xlib/src/register_types.cpp
+++ addons/godot-xlib/src/register_types.cpp
@@ -1,6 +1,6 @@
 #include "register_types.h"
 
-#include <godot/gdnative_interface.h>
+#include <gdextension_interface.h>
 
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/core/defs.hpp>
@@ -26,10 +26,10 @@ void uninitialize_xlib_module(ModuleInitializationLevel p_level) {
 
 extern "C" {
 // Initialization
-GDNativeBool GDN_EXPORT
-xlib_library_init(const GDNativeInterface *p_interface,
-                  const GDNativeExtensionClassLibraryPtr p_library,
-                  GDNativeInitialization *r_initialization) {
+GDExtensionBool GDE_EXPORT
+xlib_library_init(const GDExtensionInterface *p_interface,
+                  const GDExtensionClassLibraryPtr p_library,
+                  GDExtensionInitialization *r_initialization) {
 
   godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library,
                                                  r_initialization);
