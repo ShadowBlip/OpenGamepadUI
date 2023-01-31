45fd1594fc9f8bc63a5cbee8eb7b52d8e5a1655c
William Edwards <shadowapex@gmail.com>
2023-01-31 10:46:59 -0800

    Add device methods

--- addons/godot-evdev/src/device.cpp
+++ addons/godot-evdev/src/device.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <libevdev/libevdev-uinput.h>
 #include <libevdev/libevdev.h>
+#include <linux/input.h>
 #include <stdio.h>
 #include <unistd.h>
 
@@ -17,42 +18,89 @@
 // https://github.com/gvalkov/python-evdev/blob/2dd6ce6364bb67eedb209f6aa0bace0c18a3a40a/evdev/device.py#L1
 
 namespace evdev {
+using godot::D_METHOD;
+using godot::ERR_CANT_OPEN;
 using godot::String;
 
 InputDevice::InputDevice(){};
 InputDevice::~InputDevice() {
   // Close our file descriptors
-  if (fd > 0) {
-    ::close(fd);
-  }
+  close();
 };
 
 // Opens the given device
-int InputDevice::open(String dev) {
+int InputDevice::open(String device) {
   // Certain operations are only possible when opened in read-write mode
-  fd = ::open(dev.ascii().get_data(), O_RDWR | O_NONBLOCK);
+  fd = ::open(device.ascii().get_data(), O_RDWR | O_NONBLOCK);
   if (fd < 0) {
-    godot::UtilityFunctions::push_error("Unable to open input device");
-    return godot::ERR_CANT_OPEN;
+    godot::UtilityFunctions::push_error("Unable to open input device: ",
+                                        device);
+    return ERR_CANT_OPEN;
   }
-  path = dev;
+  int code = libevdev_new_from_fd(fd, &dev);
+  if (code < 0) {
+    ::close(fd);
+    godot::UtilityFunctions::push_error("Failed to init libevdev: ", device);
+    return ERR_CANT_OPEN;
+  }
+  path = device;
 
   return godot::OK;
 };
 
+// Close the device
+int InputDevice::close() {
+  int code = 0;
+  if (fd > 0) {
+    code = ::close(fd);
+  }
+  if (code < 0) {
+    return godot::ERR_CANT_OPEN;
+  }
+  fd = 0;
+  return code;
+};
+
+// Gets the device name
+String InputDevice::get_name() {
+  const char *name_str = libevdev_get_name(dev);
+  return String(name_str);
+}
+
 // Return path
-godot::String InputDevice::get_path() { return path; };
+String InputDevice::get_path() { return path; };
+
+// Device info functions
+int InputDevice::get_bustype() { return libevdev_get_id_bustype(dev); }
+int InputDevice::get_vendor() { return libevdev_get_id_vendor(dev); }
+int InputDevice::get_product() { return libevdev_get_id_product(dev); }
+int InputDevice::get_version() { return libevdev_get_id_version(dev); }
+
+// Returns whether the device is currently open
+bool InputDevice::is_open() { return fd > 0; };
 
 // Register the methods with Godot
 void InputDevice::_bind_methods() {
   // Properties
-  godot::ClassDB::bind_method(godot::D_METHOD("get_path"),
-                              &InputDevice::get_path);
 
   // Methods
-  godot::ClassDB::bind_method(godot::D_METHOD("open", "dev"),
-                              &InputDevice::open);
+  godot::ClassDB::bind_method(D_METHOD("open", "dev"), &InputDevice::open);
+  godot::ClassDB::bind_method(D_METHOD("close"), &InputDevice::close);
+  godot::ClassDB::bind_method(D_METHOD("get_path"), &InputDevice::get_path);
+  godot::ClassDB::bind_method(D_METHOD("get_name"), &InputDevice::get_name);
+  godot::ClassDB::bind_method(D_METHOD("get_bustype"),
+                              &InputDevice::get_bustype);
+  godot::ClassDB::bind_method(D_METHOD("get_vendor"), &InputDevice::get_vendor);
+  godot::ClassDB::bind_method(D_METHOD("get_product"),
+                              &InputDevice::get_product);
+  godot::ClassDB::bind_method(D_METHOD("get_version"),
+                              &InputDevice::get_version);
+  godot::ClassDB::bind_method(D_METHOD("is_open"), &InputDevice::is_open);
 
   // Static methods
+
+  // Constants
+  BIND_CONSTANT(EV_VERSION);
+  BIND_CONSTANT(EV_SYN);
 };
 } // namespace evdev
--- addons/godot-evdev/src/device.h
+++ addons/godot-evdev/src/device.h
@@ -4,6 +4,8 @@
 #include "godot_cpp/variant/string.hpp"
 #include <godot_cpp/core/binder_common.hpp>
 
+#include <libevdev/libevdev.h>
+
 namespace evdev {
 // Maybe inherit from Resource?
 class InputDevice : public godot::Object {
@@ -11,6 +13,8 @@ class InputDevice : public godot::Object {
 
 private:
   int fd;
+  struct libevdev *dev = NULL;
+
   godot::String path;
 
 protected:
@@ -22,10 +26,17 @@ public:
   ~InputDevice();
 
   // Properties
-  godot::String get_path();
 
-  // Member functions
+  // Methods
   int open(godot::String dev);
+  int close();
+  bool is_open();
+  godot::String get_path();
+  godot::String get_name();
+  int get_bustype();
+  int get_vendor();
+  int get_product();
+  int get_version();
 
   // Static functions
 };
