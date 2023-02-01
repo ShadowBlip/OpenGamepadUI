406fb65a1027f058b3b8d96fd1f10d5289936f15
William Edwards <shadowapex@gmail.com>
2023-01-31 22:57:41 -0800

    Add evdev module to grab exclusive gamepad access during menus

--- core/autoload/input_manager.gd
+++ core/autoload/input_manager.gd
@@ -4,15 +4,17 @@ extends Node
 var state_machine := (
 	preload("res://assets/state/state_machines/global_state_machine.tres") as StateMachine
 )
-var in_game_state := preload("res://assets/state/states/in_game.tres") as State
-var qam_state := preload("res://assets/state/states/quick_access_menu.tres") as State
-var main_menu_state := preload("res://assets/state/states/main_menu.tres") as State
 var in_game_menu_state := preload("res://assets/state/states/in_game_menu.tres") as State
+var in_game_state := preload("res://assets/state/states/in_game.tres") as State
+var main_menu_state := preload("res://assets/state/states/main_menu.tres") as State
+var qam_state := preload("res://assets/state/states/quick_access_menu.tres") as State
 var PID: int = OS.get_process_id()
 var display = OS.get_environment("DISPLAY")
 var overlay_window_id = Gamescope.get_window_id(display, PID)
-var logger := Log.get_logger("InputManager")
+var logger := Log.get_logger("InputManager", Log.LEVEL.DEBUG)
 var guide_action := false
+
+var devices := [] as Array[InputDevice]
 
 @onready var launch_manager: LaunchManager = get_node("../LaunchManager")
 
@@ -21,13 +23,89 @@ func _ready() -> void:
 	in_game_state.state_entered.connect(_on_game_state_entered)
 	in_game_state.state_exited.connect(_on_game_state_exited)
 
+	var gamepads := discover_gamepads()
+	for path in gamepads:
+		var device := InputDevice.new()
+		if device.open(path) != OK:
+			logger.warn("Unable to open gamepad: " + path)
+			continue
+		devices.append(device)
+		logger.debug("Discovered gamepad at: " + path)
+
+
+func _process(_delta: float) -> void:
+	if state_machine.current_state() == in_game_state:
+		return
+	if not state_machine.has_state(in_game_state):
+		return
+
+	# Process gamepad inputs
+	for gamepad in devices:
+		if not gamepad.is_open():
+			return
+		var events := gamepad.get_events()
+		for event in events:
+			_process_event(event)
+
+
+func _process_event(event: InputDeviceEvent) -> void:
+	if event.type == event.EV_SYN:
+		return
+	match event.get_code():
+		InputDeviceEvent.BTN_SOUTH:
+			_send_input("ogui_south", event.value == 1, 1)
+		InputDeviceEvent.BTN_NORTH:
+			_send_input("ogui_north", event.value == 1, 1)
+		InputDeviceEvent.BTN_WEST:
+			_send_input("ogui_west", event.value == 1, 1)
+		InputDeviceEvent.BTN_EAST:
+			_send_input("ogui_east", event.value == 1, 1)
+		InputDeviceEvent.BTN_MODE:
+			logger.debug("Sending ogui_guide action: " + str(event.value == 1))
+			_send_input("ogui_guide", event.value == 1, 1)
+		InputDeviceEvent.BTN_TRIGGER_HAPPY1:
+			_send_input("ui_left", event.value == 1, 1)
+		InputDeviceEvent.BTN_TRIGGER_HAPPY2:
+			_send_input("ui_right", event.value == 1, 1)
+		InputDeviceEvent.BTN_TRIGGER_HAPPY3:
+			_send_input("ui_up", event.value == 1, 1)
+		InputDeviceEvent.BTN_TRIGGER_HAPPY4:
+			_send_input("ui_down", event.value == 1, 1)
+
+
+func discover_gamepads() -> PackedStringArray:
+	var gamepads := PackedStringArray()
+	var input_path := "/dev/input"
+	var files := DirAccess.get_files_at(input_path)
+	for file in files:
+		var path := "/".join([input_path, file])
+		var dev := InputDevice.new()
+		if dev.open(path) != OK:
+			logger.debug("Unable to open event device: " + path)
+			continue
+		if dev.has_event_code(InputDeviceEvent.EV_KEY, InputDeviceEvent.BTN_MODE):
+			gamepads.append(path)
+	return gamepads
+
 
 func _on_game_state_entered(_from: State) -> void:
 	set_focus(false)
+	for device in devices:
+		if device.is_open():
+			logger.debug("Ungrabbing gamepad interception for: " + device.get_path())
+			device.grab(false)
 
 
 func _on_game_state_exited(_to: State) -> void:
 	set_focus(true)
+
+	# If no game is running now, don't intercept gamepad inputs
+	if not state_machine.has_state(in_game_state):
+		return
+	for device in devices:
+		if device.is_open():
+			logger.debug("Grabbing gamepad interception for: " + device.get_path())
+			device.grab(true)
 
 
 # Set focus will use Gamescope to focus OpenGamepadUI
