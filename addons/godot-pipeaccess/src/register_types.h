ce5518591d6f79cffdbe53236d622f8479b0c48d
William Edwards <shadowapex@gmail.com>
2023-02-14 13:51:26 -0800

    Remove pipeaccess in favor of pty

--- addons/godot-pipeaccess/src/register_types.h
+++
@@ -1,9 +0,0 @@
-#ifndef PIPEACCESS_REGISTER_TYPES_H
-#define PIPEACCESS_REGISTER_TYPES_H
-
-#include <godot_cpp/core/class_db.hpp>
-
-void initialize_pipeaccess_module(godot::ModuleInitializationLevel p_level);
-void uninitialize_pipeaccess_module(godot::ModuleInitializationLevel p_level);
-
-#endif // PIPEACCESS_REGISTER_TYPES_H
--- addons/godot-pty/src/register_types.cpp
+++ addons/godot-pty/src/register_types.cpp
@@ -6,19 +6,17 @@
 #include <godot_cpp/core/defs.hpp>
 #include <godot_cpp/godot.hpp>
 
-#include "pipeaccess.h"
 #include "pty.h"
 
-void initialize_pipeaccess_module(godot::ModuleInitializationLevel p_level) {
+void initialize_pty_module(godot::ModuleInitializationLevel p_level) {
   if (p_level != godot::MODULE_INITIALIZATION_LEVEL_SCENE) {
     return;
   }
 
-  godot::ClassDB::register_class<PipeAccess>();
   godot::ClassDB::register_class<PTY>();
 }
 
-void uninitialize_pipeaccess_module(godot::ModuleInitializationLevel p_level) {
+void uninitialize_pty_module(godot::ModuleInitializationLevel p_level) {
   if (p_level != godot::MODULE_INITIALIZATION_LEVEL_SCENE) {
     return;
   }
@@ -27,9 +25,9 @@ void uninitialize_pipeaccess_module(godot::ModuleInitializationLevel p_level) {
 extern "C" {
 // Initialization
 GDExtensionBool GDE_EXPORT
-pipeaccess_library_init(const GDExtensionInterface *p_interface,
-                        const GDExtensionClassLibraryPtr p_library,
-                        GDExtensionInitialization *r_initialization) {
+pty_library_init(const GDExtensionInterface *p_interface,
+                 const GDExtensionClassLibraryPtr p_library,
+                 GDExtensionInitialization *r_initialization) {
 
   godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library,
                                                  r_initialization);
@@ -37,8 +35,8 @@ pipeaccess_library_init(const GDExtensionInterface *p_interface,
   // Set the initializer to use and its initialization level. This controls
   // when to register and construct objects at different points of Godot's
   // initialization.
-  init_obj.register_initializer(initialize_pipeaccess_module);
-  init_obj.register_terminator(uninitialize_pipeaccess_module);
+  init_obj.register_initializer(initialize_pty_module);
+  init_obj.register_terminator(uninitialize_pty_module);
   init_obj.set_minimum_library_initialization_level(
       godot::MODULE_INITIALIZATION_LEVEL_SCENE);
 
