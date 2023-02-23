c58dc5b383edf660cacf8320921d176831135c3d
William Edwards <shadowapex@gmail.com>
2023-02-22 20:50:43 -0800

    Add platform class and OpenSD implementation

--- addons/godot-opensd/src/opensd.cpp
+++ addons/godot-opensd/src/opensd.cpp
@@ -1,3 +1,5 @@
+#include "godot_cpp/classes/dir_access.hpp"
+#include "godot_cpp/classes/file_access.hpp"
 #include "godot_cpp/classes/global_constants.hpp"
 #include "godot_cpp/variant/packed_byte_array.hpp"
 #include "godot_cpp/variant/packed_string_array.hpp"
@@ -5,12 +7,23 @@
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/variant/utility_functions.hpp>
 
+#include "cmake_vars.hpp"
 #include "common/log.hpp"
 #include "opensdd/daemon.hpp"
 
 #include "opensd.h"
 
-OpenSD::OpenSD(){};
+OpenSD::OpenSD() {
+  // Create the OpenSD data directories
+  char *data_dir = const_cast<char *>(CMakeVar::INSTALL_DATA_DIR.c_str());
+  godot::DirAccess::make_dir_recursive_absolute(data_dir);
+  char *config_dir =
+      const_cast<char *>(CMakeVar::INSTALL_DATA_CONFIG_DIR.c_str());
+  godot::DirAccess::make_dir_recursive_absolute(config_dir);
+  char *profile_dir =
+      const_cast<char *>(CMakeVar::INSTALL_DATA_PROFILE_DIR.c_str());
+  godot::DirAccess::make_dir_recursive_absolute(profile_dir);
+};
 OpenSD::~OpenSD(){};
 
 // Creates and opens a new OpenSD
@@ -22,6 +35,8 @@ int OpenSD::run() {
   } catch (int code) {
     // Failed to run
     godot::UtilityFunctions::push_error("OpenSD exited with code: ", code);
+    return godot::ERR_CANT_OPEN;
+  } catch (...) {
     return godot::ERR_CANT_OPEN;
   }
 }
