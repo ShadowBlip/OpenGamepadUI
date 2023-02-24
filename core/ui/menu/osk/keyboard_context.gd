29ee4ee14f1268d9458322f7d251119b0e5b2cfb
William Edwards <shadowapex@gmail.com>
2023-02-23 22:36:54 -0800

    Implement mapping keyboard keys using OSK and refactor OSK resources

--- core/ui/menu/osk/keyboard_context.gd
+++ core/ui/menu/osk/keyboard_context.gd
@@ -1,18 +1,36 @@
 extends Resource
 class_name KeyboardContext
 
+## Route on-screen keyboard input
+##
+## A KeyboardContext defines how the on-screen keyboard should route its key
+## input. If the context type is set to TYPE.GODOT, the keyboard will update
+## the given control node's text from the keyboard. If it is TYPE.X11, it will
+## send virtual key pressess to the game via Xlib/evdev.
+
+## Type of Keyboard context
+# TODO: Break these up into their own classes
 enum TYPE {
-	GODOT,
-	X11,
+	GODOT,  ## Routes keyboard input to a Godot control node (i.e. textbox)
+	X11,  ## Routes keyboard input to the currently running game
+	INPUT_MAPPER,  ## Routes the next selected key to a GamepadMapping
 }
 
+## Emitted when the on-screen keyboard submits this context
 signal submitted
+## Emitted when the keyboard is opened with this context
 signal entered
+## Emitted when the keyboard is closed with this context
 signal exited
 
+## The type of keyboard context
 var type: TYPE
+## For non-TYPE.X11 contexts, which node to send text input to
 var target: Control
+## Whether or not the keyboard should close after submition
 var close_on_submit: bool = true
+## If this is a TYPE.INPUT_MAPPER, then it is the GamepadMapping to update
+var mapping: GamepadMapping
 
 
 func _init(t: TYPE = TYPE.GODOT, tgt: Control = null, close_after_submit: bool = true) -> void:
--- core/ui/menu/osk/on_screen_keyboard.gd
+++ core/ui/menu/osk/on_screen_keyboard.gd
@@ -15,12 +15,12 @@ enum MODE_SHIFT {
 	ONE_SHOT,
 }
 
-@export var layout: KeyboardLayout = KeyboardLayout.new()
+@export var layout: KeyboardLayout
 @export var instance: KeyboardInstance = preload("res://core/global/keyboard_instance.tres")
 var _mode_shift: MODE_SHIFT = MODE_SHIFT.OFF
 var logger := Log.get_logger("OSK")
 
-@onready var rows_container := $MarginContainer/VBoxContainer
+@onready var rows_container := $%KeyboardRowsContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -40,7 +40,7 @@ func populate_keyboard() -> void:
 	
 	# Populate the keyboard keys based on the given layout
 	for r in (layout as KeyboardLayout).rows:
-		var row := r as Array
+		var row := r as KeyboardRow
 		
 		# Create an HBox Container for the row
 		var container := HBoxContainer.new()
@@ -48,16 +48,18 @@ func populate_keyboard() -> void:
 		rows_container.add_child(container)
 		
 		# Loop through the layout and create key buttons for each key
-		for k in row:
+		for k in row.entries:
 			var key := k as KeyboardKeyConfig
 			if not key:
 				continue
+
+			# Create a button instance for this key
 			var button := key_scene.instantiate()
 			button.size_flags_stretch_ratio = key.stretch_ratio
-			if key.type == KeyboardKeyConfig.TYPE.CHAR and key.display != "":
-				button.text = key.display
-			elif key.type == KeyboardKeyConfig.TYPE.SPECIAL and key.icon == null:
-				button.text = key.display
+
+			# Get the text to display for this key
+			var display := key.get_text()
+			button.text = display
 			
 			# Connect the keyboard key to a method to handle key presses
 			button.button_up.connect(_on_key_pressed.bind(key))
@@ -65,9 +67,9 @@ func populate_keyboard() -> void:
 			# Create a callback when the keyboard mode changes (i.e. shift is pressed)
 			var on_mode_shift := func _on_mode_shift(mode: MODE_SHIFT) -> void:
 				if mode > MODE_SHIFT.OFF:
-					button.text = key.display_uppercase
+					button.text = key.get_text(true)
 					return
-				button.text = key.display
+				button.text = key.get_text()
 			mode_shifted.connect(on_mode_shift)
 			
 			container.add_child(button)
@@ -108,6 +110,7 @@ func set_mode_shift(mode: MODE_SHIFT) -> void:
 	mode_shifted.emit(mode)
 
 
+# Handle gamepad keyboard shortcuts
 func _input(event: InputEvent) -> void:
 	if not visible:
 		return
@@ -117,8 +120,8 @@ func _input(event: InputEvent) -> void:
 		return 
 	if event.is_action_released("ogui_north"):
 		var key := KeyboardKeyConfig.new()
-		key.type = key.TYPE.SPECIAL
-		key.action = key.ACTION.BKSP
+		key.input = InputEventKey.new()
+		key.input.keycode = KEY_BACKSPACE
 		_on_key_pressed(key)
 		get_viewport().set_input_as_handled()
 		return
@@ -134,8 +137,8 @@ func _input(event: InputEvent) -> void:
 		if event.get_action_strength("ogui_right_trigger") < 0.5:
 			return
 		var key := KeyboardKeyConfig.new()
-		key.type = key.TYPE.SPECIAL
-		key.action = key.ACTION.ENTER
+		key.input = InputEventKey.new()
+		key.input.keycode = KEY_ENTER
 		_on_key_pressed(key)
 		return
 
@@ -150,33 +153,44 @@ func _gui_input(event: InputEvent) -> void:
 # Handle all kinds of key presses. Key inputs get processed depending on the
 # currently set keyboard context.
 func _on_key_pressed(key: KeyboardKeyConfig) -> void:
-	if key.type == KeyboardKeyConfig.TYPE.CHAR:
-		_handle_key_char(key)
+	if not instance.context:
+		logger.warn("Keyboard context not set, nowhere to send key input.")
 		return
-	_handle_key_special(key)
+
+	match instance.context.type:
+		KeyboardContext.TYPE.GODOT:
+			_handle_native(key)
+			return
+		KeyboardContext.TYPE.INPUT_MAPPER:
+			_handle_input_map(key)
+			return
+		KeyboardContext.TYPE.X11:
+			return
 
 
 # Handles normal character inputs
-func _handle_key_char(key: KeyboardKeyConfig) -> void:
-	if instance.context == null:
-		logger.warn("Keyboard context not set, nowhere to send key input.")
-		return
-	
-	if instance.context.type == KeyboardContext.TYPE.X11:
-		return
-	
+func _handle_native(key: KeyboardKeyConfig) -> void:
 	if instance.context.target == null:
 		logger.warn("Keyboard target not set, nowhere to send key input.")
 		return
 	var target = instance.context.target
 
+	# Get the input event based on mode shift
+	var event := key.input
+	if _mode_shift > MODE_SHIFT.OFF and key.mode_shift_input:
+		event = key.mode_shift_input
+
 	# Get the character to send to the target
-	var character := key.output
-	if _mode_shift > MODE_SHIFT.OFF:
-		character = key.output_uppercase
-		if _mode_shift == MODE_SHIFT.ONE_SHOT:
-			set_mode_shift(MODE_SHIFT.OFF)
+	var character := String.chr(event.unicode)
+	if _mode_shift == MODE_SHIFT.ONE_SHOT:
+		set_mode_shift(MODE_SHIFT.OFF)
 	
+	# If no character was found for the key, then it is a special character
+	# like shift, alt, etc.
+	if character == "":
+		_handle_native_action(key)
+		return
+
 	# Update the target text with the given character
 	if target is TextEdit:
 		var text_edit := instance.context.target as TextEdit
@@ -191,28 +205,25 @@ func _handle_key_char(key: KeyboardKeyConfig) -> void:
 	logger.warn("Keyboard target is not a supported type. Can't send key input.")
 
 
-# Handles special key inputs like shift, alt, ctrl, etc.
-func _handle_key_special(key: KeyboardKeyConfig) -> void:
+# Handles action key inputs like shift, alt, ctrl, etc.
+func _handle_native_action(key: KeyboardKeyConfig) -> void:
 	if not instance.context:
 		return
 	var target = instance.context.target
-	match key.action:
-		KeyboardKeyConfig.ACTION.SHIFT:
+	match key.input.keycode:
+		KEY_SHIFT:
 			if _mode_shift > MODE_SHIFT.OFF:
 				set_mode_shift(MODE_SHIFT.OFF)
 				return
 			set_mode_shift(MODE_SHIFT.ONE_SHOT)
 			return
-		KeyboardKeyConfig.ACTION.CAPS:
+		KEY_CAPSLOCK:
 			if _mode_shift > MODE_SHIFT.OFF:
 				set_mode_shift(MODE_SHIFT.OFF)
 				return
 			set_mode_shift(MODE_SHIFT.ON)
 			return
-		KeyboardKeyConfig.ACTION.CLOSE_KEYBOARD:
-			instance.close()
-			return
-		KeyboardKeyConfig.ACTION.BKSP:
+		KEY_BACKSPACE:
 			if target != null and target is TextEdit:
 				var text_edit := target as TextEdit
 				text_edit.backspace()
@@ -220,8 +231,33 @@ func _handle_key_special(key: KeyboardKeyConfig) -> void:
 				var line_edit := target as LineEdit
 				line_edit.delete_char_at_caret()
 			return
-		KeyboardKeyConfig.ACTION.ENTER:
+		KEY_ENTER:
 			instance.context.submitted.emit()
 			if instance.context.close_on_submit:
 				instance.close()
 			return
+
+
+# Handle special keys that are not part of a real keyboard
+func _handle_special(key: KeyboardKeyConfig) -> void:
+	if key.action == key.ACTION.CLOSE_KEYBOARD:
+		instance.close()
+		return
+
+
+# Handle cases where the KeyboardContext is set to INPUT_MAPPING
+func _handle_input_map(key: KeyboardKeyConfig) -> void:
+	# Get the input event based on mode shift
+	var event := key.input
+	if _mode_shift > MODE_SHIFT.OFF and key.mode_shift_input:
+		event = key.mode_shift_input
+
+	# Handle cases where the user wants to select a mode-shifted key. 
+	# If the user selects SHIFT twice, select shift as the input
+	if event.keycode == KEY_SHIFT and _mode_shift == MODE_SHIFT.OFF:
+		set_mode_shift(MODE_SHIFT.ONE_SHOT)
+		return
+
+	instance.context.mapping.target = event
+	set_mode_shift(MODE_SHIFT.OFF)
+	instance.close()
