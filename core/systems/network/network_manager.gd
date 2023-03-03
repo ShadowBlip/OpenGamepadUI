e18e625e414e36b7609000b37aa9d7f3f63244de
William Edwards <shadowapex@gmail.com>
2023-03-03 12:52:33 -0800

    Add network settings menu (#63)
    * Add NetworkManager class
    
    * Add network settings menu and display available networks
    
    * Add accurate wifi signal to top bar
    
    * Add wifi password challenge dialog
    
    * Lower system thread tick rate and wire up OSK submit for wifi password
    
    * Fix back button presses when password dialog is open

---
+++ core/systems/network/network_manager.gd
@@ -0,0 +1,147 @@
+extends Object
+class_name NetworkManager
+
+## Manage and interact with the system network settings
+##
+## Allows network management through nmcli
+## Reference: https://developer-old.gnome.org/NetworkManager/stable/nmcli.html
+
+const bar_0 := preload("res://assets/ui/icons/wifi-none.svg")
+const bar_1 := preload("res://assets/ui/icons/wifi-low.svg")
+const bar_2 := preload("res://assets/ui/icons/wifi-medium.svg")
+const bar_3 := preload("res://assets/ui/icons/wifi-high.svg")
+
+const common_args := ["--terse", "--color", "no"]
+
+
+## Wireless Access Point
+class WifiAP:
+	var in_use: bool
+	var bssid: String
+	var ssid: String
+	var mode: String
+	var channel: int
+	var rate: String
+	var strength: int
+	var security: String
+
+
+## Network device
+class NetworkDevice:
+	var device: String
+	var type: String
+	var state: String
+	var connection: String
+
+
+## Returns true if the system has network controls we support
+static func supports_network() -> bool:
+	var code := OS.execute("which", ["nmcli"])
+	return code == 0
+
+
+## Returns a list of network devices
+#$nmcli --terse --color no device
+#enp5s0:ethernet:connected:Wired connection 1
+#wlp4s0:wifi:connected:Chonenberg
+#lo:loopback:connected (externally):lo
+#p2p-dev-wlp4s0:wifi-p2p:disconnected:
+static func get_devices() -> Array[NetworkDevice]:
+	var result: Array[NetworkDevice] = []
+	var output := _run_nmcli(["device"])
+	for line in output:
+		var device := NetworkDevice.new()
+		device.device = line[0]
+		device.type = line[1]
+		device.state = line[2]
+		device.connection = line[3]
+		result.append(device)
+
+	return result
+
+
+## Returns a list of available wifi access points
+#$ nmcli --terse --color no dev wifi
+# :AA\:BB\:CC\:83\:82\:FF:Chronenberg 5GHz:Infra:120:405 Mbit/s:94:▂▄▆█:WPA2
+#*:AA\:BB\:CC\:83\:82\:FB:Chonenberg:Infra:11:195 Mbit/s:83:▂▄▆█:WPA2
+static func get_access_points() -> Array[WifiAP]:
+	var result: Array[WifiAP] = []
+	var output := _run_nmcli(["dev", "wifi"])
+	for line in output:
+		var ap := WifiAP.new()
+		ap.in_use = line[0] == "*"
+		ap.bssid = line[1]
+		ap.ssid = line[2]
+		ap.mode = line[3]
+		ap.channel = line[4].to_int()
+		ap.rate = line[5]
+		ap.strength = line[6].to_int()
+		ap.security = line[8]
+		result.append(ap)
+
+	return result
+
+
+## Returns the currently connected access point
+static func get_current_access_point() -> WifiAP:
+	var access_points := get_access_points()
+	for ap in access_points:
+		if ap.in_use:
+			return ap
+	return null
+
+
+## Connect to the given wifi access point
+static func connect_access_point(ssid: String, password: String = "") -> int:
+	var args := ["dev", "wifi", "connect", ssid]
+	if password != "":
+		args.append_array(["password", password])
+	var output := []
+	var code := OS.execute("nmcli", args, output)
+	if code != OK:
+		push_warning("Unable to connect to ", ssid, ": ", output[0])
+	return code
+
+
+## Returns the texture reflecting the given wifi strength
+static func get_strength_texture(strength: int) -> Texture2D:
+	if strength >= 80:
+		return bar_3
+	if strength >= 60:
+		return bar_2
+	if strength >= 40:
+		return bar_1
+	return bar_0
+
+
+# Run nmcli with the given arguments. Returns the parsed output.
+static func _run_nmcli(args: PackedStringArray) -> Array[PackedStringArray]:
+	var output := []
+	var cmd_args := common_args.duplicate()
+	cmd_args.append_array(args)
+	var code := OS.execute("nmcli", cmd_args, output)
+	if code != OK:
+		return []
+
+	return _parse_nmcli(output[0])
+
+
+# Parses the terse output of nmcli, which is separated by ':'
+#$ nmcli --terse --color no dev wifi
+# :AA\:BB\:CC\:83\:82\:FF:Chronenberg 5GHz:Infra:120:405 Mbit/s:94:▂▄▆█:WPA2
+#*:AA\:BB\:CC\:83\:82\:FB:Chonenberg:Infra:11:195 Mbit/s:83:▂▄▆█:WPA2
+static func _parse_nmcli(output: String) -> Array[PackedStringArray]:
+	var parsed: Array[PackedStringArray] = []
+	var lines := output.split("\n")
+	for line in lines:
+		var parsed_line := PackedStringArray()
+		if line == "":
+			continue
+		line = line.replace("\\:", "%COLON%")
+		var columns := line.split(":")
+		for column in columns:
+			column = column.replace("%COLON%", ":")
+			parsed_line.append(column)
+		parsed.append(parsed_line)
+
+	return parsed
--- core/ui/menu/navigation/top_bar.gd
+++ core/ui/menu/navigation/top_bar.gd
@@ -1,8 +1,10 @@
 extends Control
 
-var  main_menu_state := preload("res://assets/state/states/main_menu.tres") as State
-var  in_game_menu_state := preload("res://assets/state/states/in_game_menu.tres") as State
-var  qam_state := preload("res://assets/state/states/quick_access_menu.tres") as State
+const system_thread := preload("res://core/systems/threading/system_thread.tres")
+
+var main_menu_state := preload("res://assets/state/states/main_menu.tres") as State
+var in_game_menu_state := preload("res://assets/state/states/in_game_menu.tres") as State
+var qam_state := preload("res://assets/state/states/quick_access_menu.tres") as State
 
 var panel_alpha_normal := 156
 var panel_alpha_solid := 255
@@ -12,9 +14,13 @@ var panel_alpha_solid := 255
 @onready var battery_label: Label = $%BatteryLabel
 @onready var search_bar := $%SearchBar
 @onready var panel := $%Panel
+@onready var network_icon := $%NetworkIcon as TextureRect
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	system_thread.start()
+
 	# Update the panel alpha during in-game and main menus
 	panel.modulate.a8 = panel_alpha_normal
 	main_menu_state.state_entered.connect(_on_game_menu_entered)
@@ -24,14 +30,22 @@ func _ready() -> void:
 	qam_state.state_entered.connect(_on_game_menu_entered)
 	qam_state.state_exited.connect(_on_game_menu_exited)
 
-	# Create a timer to update the time and battery percent
+	# Create a timer to update the time
 	var time_timer: Timer = Timer.new()
 	time_timer.timeout.connect(_on_update_time)
-	time_timer.timeout.connect(_on_update_battery)
 	time_timer.autostart = true
 	add_child(time_timer)
 	_on_update_time()
 	_on_update_battery()
+
+	# Create a timer to check wifi signal strength and battery percent
+	var wifi_timer := Timer.new()
+	wifi_timer.timeout.connect(_on_wifi_update)
+	wifi_timer.timeout.connect(_on_update_battery)
+	wifi_timer.wait_time = 60
+	wifi_timer.autostart = true
+	add_child(wifi_timer)
+	_on_wifi_update()
 
 
 func _on_game_menu_entered(_from: State) -> void:
@@ -44,9 +58,22 @@ func _on_game_menu_exited(to: State) -> void:
 	panel.modulate.a8 = panel_alpha_normal
 
 
+# Updates the wifi signal strength on timer timeout
+func _on_wifi_update() -> void:
+	var get_ap := func() -> NetworkManager.WifiAP:
+		return NetworkManager.get_current_access_point()
+	var current_ap := await system_thread.exec(get_ap) as NetworkManager.WifiAP
+	var strength := 0
+	if current_ap:
+		strength = current_ap.strength
+	network_icon.texture = NetworkManager.get_strength_texture(strength)
+
+
 # Updates the battery capacity on timer timeout
 func _on_update_battery():
-	var current_capacity: int = Battery.get_capacity(battery)
+	var get_capacity := func() -> int:
+		return Battery.get_capacity(battery)
+	var current_capacity: int = await system_thread.exec(get_capacity)
 	battery_label.text = "{0}%".format([current_capacity])
 
 
@@ -55,17 +82,17 @@ func _on_update_time():
 	# year, month, day, weekday, hour, minute, second
 	var time = Time.get_datetime_dict_from_system()
 	time_label.text = _format_time(time)
-	
-	
+
+
 func _format_time(time: Dictionary) -> String:
 	time["meridium"] = "am"
 	if time["hour"] > 11:
 		time["meridium"] = "pm"
 	if time["hour"] > 12:
 		time["hour"] -= 12
-	
+
 	# Pad our minutes and hours
 	time["minute"] = "%02d" % time["minute"]
 	time["hour"] = "%02d" % time["hour"]
-	
+
 	return "{hour}:{minute}{meridium}".format(time)
