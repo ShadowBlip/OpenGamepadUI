104d5ecc4a48e31f4f5910555a37e56e32e9e0ab
William Edwards <shadowapex@gmail.com>
2023-03-03 17:28:38 -0800

    Add start of platform providers

--- core/platform/platform_provider.gd
+++ core/platform/platform_provider.gd
@@ -1,2 +1,9 @@
 extends Resource
 class_name PlatformProvider
+
+@export var name: String
+var logger := Log.get_logger("PlatformProvider", Log.LEVEL.DEBUG)
+
+
+func get_handheld_gamepad() -> HandheldGamepad:
+	return null
