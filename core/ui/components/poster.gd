0dbfaa290adb2f98bb6b2bd03d4c793d0d050553
William Edwards <shadowapex@gmail.com>
2023-03-08 21:06:06 -0800

    Better touch/mouse support

--- core/ui/components/poster.gd
+++ core/ui/components/poster.gd
@@ -8,7 +8,7 @@ enum LAYOUT_MODE {
 
 const LAYOUTS: Dictionary = {
 	LAYOUT_MODE.LANDSCAPE: Vector2(460, 215),  # Original: 460 x 215
-	LAYOUT_MODE.PORTRAIT: Vector2(143, 215), # Original: 600 x 900
+	LAYOUT_MODE.PORTRAIT: Vector2(143, 215),  # Original: 600 x 900
 }
 
 @export var text: String = "Empty"
@@ -24,29 +24,32 @@ var _select_audio_stream = load(select_audio)
 @onready var _panel: Panel = $Label/Panel
 @onready var _panel_alpha: int = _panel.modulate.a8
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_set_layout(LAYOUTS[layout])
 	_label.text = text
 	_highlight(false)
-	
+
 	# Connect to focus events
 	focus_entered.connect(_play_sound.bind(_focus_audio_stream))
 	focus_entered.connect(_highlight.bind(true))
+	mouse_entered.connect(_highlight.bind(true))
 	focus_exited.connect(_highlight.bind(false))
+	mouse_exited.connect(_highlight.bind(false))
 	pressed.connect(_play_sound.bind(_select_audio_stream))
 
 
 # Transforms our poster when we are highlighted or not
 func _highlight(focused: bool) -> void:
 	if focused:
-		var focused_alpha = _panel_alpha*1.5
+		var focused_alpha = _panel_alpha * 1.5
 		_panel.modulate.a8 = focused_alpha
 		self_modulate.r = 1.15
 		self_modulate.g = 1.15
 		self_modulate.b = 1.15
 		return
-	
+
 	_panel.modulate.a8 = _panel_alpha
 	self_modulate.r = 0.85
 	self_modulate.g = 0.85
