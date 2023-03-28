23e47feb7edad62dc6f4eb58d4be5557b244a162
William Edwards <shadowapex@gmail.com>
2023-03-27 19:08:28 -0700

    Push the library state when search bar is activated

--- core/ui/components/search_bar.gd
+++ core/ui/components/search_bar.gd
@@ -3,8 +3,12 @@ class_name SearchBar
 
 signal search_submitted(text: String)
 
-@export var keyboard: KeyboardInstance = preload("res://core/global/keyboard_instance.tres")
+var state_machine := load("res://assets/state/state_machines/global_state_machine.tres") as StateMachine
+var library_state := load("res://assets/state/states/library.tres")
+
 var keyboard_context := KeyboardContext.new(KeyboardContext.TYPE.GODOT, self)
+@export var keyboard: KeyboardInstance = preload("res://core/global/keyboard_instance.tres")
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -20,5 +24,10 @@ func _on_text_changed() -> void:
 func _gui_input(event: InputEvent) -> void:
 	if not has_focus():
 		return
-	if event.is_action_released("ogui_south"):
-		keyboard.open(keyboard_context)
+	if not event.is_action_released("ogui_south"):
+		return
+	
+	if state_machine.current_state() != library_state:
+		state_machine.push_state(library_state)
+		
+	keyboard.open(keyboard_context)
