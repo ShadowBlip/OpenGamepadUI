69cdd3d64c04cf4f888b3000536a8de02e1d34aa
William Edwards <shadowapex@gmail.com>
2023-03-28 21:40:32 -0700

    Fix display manager and finish implementing software updater

--- core/systems/updater/software_updater.gd
+++ core/systems/updater/software_updater.gd
@@ -1,30 +1,38 @@
 extends Node
 class_name SoftwareUpdater
 
-signal update_available
+signal update_available(available: bool)
+signal update_installed(status: int)
 
 var Version := preload("res://core/global/version.tres") as Version
 var update_pack_url := ""
-var logger := Log.get_logger("SoftwareUpdater")
+var logger := Log.get_logger("SoftwareUpdater", Log.LEVEL.DEBUG)
 
 @export var github_project := "ShadowBlip/OpenGamepadUI"
+@export var update_filename := "update.pck"
+@export var update_hash_filename := "update.pck.sha256.txt"
+@export var update_folder := "user://updates"
 
 @onready var github_client := $GitHubClient as GitHubClient
 
 
 ## Checks to see if there is a newer version of OpenGamepadUI available.
 func check_for_updates() -> void:
+	logger.info("Checking for OpenGamepadUI updates...")
 	# Fetch the latest release from GitHub
 	var response = await github_client.get_releases(github_project, 1)
 	if response == null:
 		logger.info("Unable to check for software updates")
+		update_available.emit(false)
 		return
-	var releases := response as Array[Dictionary]
+	var releases := response as Array
 	if releases.size() == 0:
+		update_available.emit(false)
 		return
-	var latest_release := releases[0]
+	var latest_release := releases[0] as Dictionary
 	if not "tag_name" in latest_release:
 		logger.info("No tag name in release")
+		update_available.emit(false)
 		return
 	var tag := latest_release["tag_name"] as String
 	tag = tag.replace("v", "")
@@ -32,11 +40,13 @@ func check_for_updates() -> void:
 	# Check if the release is newer than the current one.
 	if not _is_greater_version(Version.core, tag):
 		logger.debug("Latest release " + tag + " is not newer than installed: " + Version.core)
+		update_available.emit(false)
 		return
 	
 	# Look for the update pack in the release assets
 	if not "assets" in latest_release:
 		logger.info("No assets found in release")
+		update_available.emit(false)
 		return
 	var assets := latest_release["assets"] as Array[Dictionary]
 	
@@ -46,14 +56,64 @@ func check_for_updates() -> void:
 			continue
 		if not "browser_download_url" in asset:
 			continue
-		if asset["name"] == "update.pck":
+		if asset["name"] == update_filename:
 			update_pack_url = asset["browser_download_url"]
 	
 	if update_pack_url == "":
-		logger.info("Unable to find update.pck in release assets")
+		logger.info("Unable to find update pack in release assets")
+		update_available.emit(false)
 		return
 	
-	update_available.emit()
+	update_available.emit(true)
+
+
+## Downloads and installs the given update
+func install_update(download_url: String, sha256: String = "") -> void:
+	# Build the request
+	var http: HTTPRequest = HTTPRequest.new()
+	add_child(http)
+	if http.request(download_url) != OK:
+		logger.info("Error making http request for update pack: " + download_url)
+		http.queue_free()
+		update_installed.emit(FAILED)
+		return
+
+	# Wait for the request signal to complete
+	# result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray
+	var args: Array = await http.request_completed
+	var result: int = args[0]
+	var response_code: int = args[1]
+	var headers: PackedStringArray = args[2]
+	var body: PackedByteArray = args[3]
+	http.queue_free()
+
+	if result != HTTPRequest.RESULT_SUCCESS:
+		logger.info("Update couldn't be downloaded: " + download_url)
+		update_installed.emit(FAILED)
+		return
+
+	# Now we have the body ;)
+	var ctx = HashingContext.new()
+
+	# Start a SHA-256 context.
+	ctx.start(HashingContext.HASH_SHA256)
+	ctx.update(body)
+
+	# Get the computed hash.
+	var res = ctx.finish()
+
+	# Print the result as hex string and array.
+	if sha256 != "" and res.hex_encode() != sha256:
+		logger.error("sha256 hash does not match for the downloaded update pack")
+		update_installed.emit(FAILED)
+		return
+
+	# Install the update
+	DirAccess.make_dir_recursive_absolute(update_folder)
+	var file := FileAccess.open("/".join([update_folder, update_filename]), FileAccess.WRITE_READ)
+	file.store_buffer(body)
+	file.close()
+	update_installed.emit(OK)
 
 
 # Returns whether or not the given semantic version string is greater than 
--- core/ui/menu/settings/general_settings_menu.gd
+++ core/ui/menu/settings/general_settings_menu.gd
@@ -1,10 +1,86 @@
 extends Control
 
 var SettingsManager := load("res://core/global/settings_manager.tres") as SettingsManager
+var NotificationManager := load("res://core/global/notification_manager.tres") as NotificationManager
+var update_available := false
+var update_installed := false
+var logger := Log.get_logger("GeneralSettings")
+
+@onready var updater := $SoftwareUpdater as SoftwareUpdater
+@onready var update_timer := $UpdateTimer as Timer
+@onready var auto_update_toggle := $%AutoUpdateToggle
+@onready var check_update_button := $%CheckUpdateButton
+@onready var update_button := $%UpdateButton
 @onready var max_recent_slider := $%MaxRecentAppsSlider
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Configure home menu
 	var max_recent := SettingsManager.get_value("general.home", "max_home_items", 10) as int
 	max_recent_slider.value = max_recent
+
+	# Configure check updates
+	var on_check_updates := func():
+		check_update_button.disabled = true
+		updater.check_for_updates()
+		var available := await updater.update_available as bool
+		check_update_button.disabled = false
+		if not available:
+			var notify := Notification.new("Client is already up to date")
+			NotificationManager.show(notify)
+			return
+		var notify := Notification.new("New client update is available")
+		NotificationManager.show(notify)
+		update_available = true
+		update_button.disabled = false
+	check_update_button.pressed.connect(on_check_updates)
+
+	# Configure install update
+	var on_install_update := func():
+		update_button.disabled = true
+		updater.install_update(updater.update_pack_url)
+		var status := await updater.update_installed as int
+		if status == OK:
+			var notify := Notification.new("Client update installed successfully")
+			NotificationManager.show(notify)
+			update_installed = true
+			return
+		update_button.disabled = false
+		var notify := Notification.new("Failed to install client update")
+		NotificationManager.show(notify)
+	update_button.pressed.connect(on_install_update)
+
+	# Configure auto updates
+	var auto_update := SettingsManager.get_value("general.updates", "auto_update", false) as bool
+	auto_update_toggle.button_pressed = auto_update
+	var on_auto_update_toggled := func(toggled: bool):
+		SettingsManager.set_value("general.updates", "auto_update", toggled)
+		if toggled:
+			update_timer.start()
+		else:
+			update_timer.stop()
+	auto_update_toggle.toggled.connect(on_auto_update_toggled)
+	update_timer.timeout.connect(_on_autoupdate)
+	if auto_update:
+		update_timer.start()
+
+
+func _on_autoupdate() -> void:
+	logger.info("Automatically checking for updates...")
+	check_update_button.disabled = true
+	updater.check_for_updates()
+	var available := await updater.update_available as bool
+	check_update_button.disabled = false
+	if not available:
+		logger.info("No new updates available")
+		return
+
+	logger.info("New update was found. Trying to install it.")
+	update_button.disabled = true
+	updater.install_update(updater.update_pack_url)
+	var status := await updater.update_installed as int
+	if status == OK:
+		update_installed = true
+		return
+	update_button.disabled = false
