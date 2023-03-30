118a0fbea89025066349d49946ba781b8b6e5250
William Edwards <shadowapex@gmail.com>
2023-03-30 15:02:49 -0700

    feat(build): Remove source addons in favor of their pre-built binaries

--- addons/godot-evdev/src/register_types.h
+++
@@ -1,9 +0,0 @@
-#ifndef EVDEV_REGISTER_TYPES_H
-#define EVDEV_REGISTER_TYPES_H
-
-#include <godot_cpp/core/class_db.hpp>
-
-void initialize_evdev_module(godot::ModuleInitializationLevel p_level);
-void uninitialize_evdev_module(godot::ModuleInitializationLevel p_level);
-
-#endif // EVDEV_REGISTER_TYPES_H
--- addons/godot-opensd/src/register_types.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPENSD_REGISTER_TYPES_H
-#define OPENSD_REGISTER_TYPES_H
-
-#include <godot_cpp/core/class_db.hpp>
-
-void initialize_opensd_module(godot::ModuleInitializationLevel p_level);
-void uninitialize_opensd_module(godot::ModuleInitializationLevel p_level);
-
-#endif // OPENSD_REGISTER_TYPES_H
