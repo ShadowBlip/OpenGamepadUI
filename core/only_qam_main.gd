41fcafe3057157b8a5eb7450c54816f69a307feb
Derek J. Clark <derekjohn.clark@gmail.com>
2023-03-31 16:32:36 -0700

    Add visibility manager to QAM/OnlyQAM and remove GDScript visibility management. Rename only_qam to only_qam_main. Adjust margins for QAM/OnlyQAM scene and add V separator between icon bar and viewport. Use editor nodes instead of GDScipt for InputManager and QuickAccessMenu in only_qam_main. Add support for running a different child process in only_qam mode. Minor refactor of only_qam_main.gd with better code comments, modular functions, and variable names.

--- core/only_qam_main.gd
+++ core/only_qam_main.gd
@@ -9,14 +9,14 @@ var display := Gamescope.XWAYLAND.OGUI
 var qam_window_id: int
 var pid: int
 var shared_thread: SharedThread
-var steam_log: FileAccess
-var steam_process: InteractiveProcess
-var steam_window_id: int
+var underlay_log: FileAccess
+var underlay_process: InteractiveProcess
+var underlay_window_id: int
 
-var logger := Log.get_logger("OQMain", Log.LEVEL.INFO)
+var logger := Log.get_logger("Main-OnlyQAM", Log.LEVEL.INFO)
 
+## Sets up PluginManager for QAM only mode.
 func _init():
-	logger.debug("Init only_qam mode.")
 	var plugin_loader := load("res://core/global/plugin_loader.tres") as PluginLoader
 	var filters : Array[Callable] = [plugin_loader.filter_by_tag.bind("qam")]
 	plugin_loader.set_plugin_filters(filters)
@@ -27,67 +27,44 @@ func _init():
 
 ## Starts the --only-qam/--qam-only session.
 func _ready() -> void:
-	shared_thread = SharedThread.new()
-	shared_thread.start()
 	# Set window size to native resolution
 	var screen_size : Vector2i = DisplayServer.screen_get_size()
 	var window : Window = get_window()
 	window.set_size(screen_size)
 
-	var steam_log_path = OS.get_environment("HOME") + "/.steam-stdout.log"
-	steam_log = FileAccess.open(steam_log_path, FileAccess.WRITE)
-	var error := steam_log.get_open_error()
-	if error != OK:
-		logger.warn("Got error opening log file.")
-	else:
-		logger.debug("Opened Steam log at " + steam_log_path)
-	# Get user arguments
+	# Set up the session
 	var args := OS.get_cmdline_user_args()
 	_setup_qam_only(args)
 
 
-## Creates teh input manager, qam scene, and starts teh user defined program (steam) in firejail.
+## Finds needed PID's and global vars, Starts the user defined program in the sandbox.
 func _setup_qam_only(args: Array) -> void:
-	# Setup input manager
-	var input_scene := load("res://core/systems/input/only_qam_input_manager.tscn") as PackedScene
-	add_child(input_scene.instantiate())
-
-	# Add the QAM
-	var qam_scene := load("res://core/ui/menu/qam/only_quick_access_menu.tscn") as PackedScene
-	var qam := qam_scene.instantiate()
-	add_child(qam)
-	qam.remove_child(qam.get_node("BackInputHandler"))
-
 	pid = OS.get_process_id()
 	qam_window_id = Gamescope.get_window_id(pid, display)
 	qam_state.state_entered.connect(_on_qam_open)
 	qam_state.state_exited.connect(_on_qam_closed)
-	
+
 	InputManager._set_intercept(ManagedGamepad.INTERCEPT_MODE.PASS_QAM)
 
 	# Don't crash if we're not launching another program.
 	if args == []:
 		logger.warn("only-qam mode started with no launcher arguments.")
 		return
-	# Launch underlay in the sandbox 
-	var sandbox := PackedStringArray()
-	sandbox.append_array(["--noprofile"])
-	var blacklist := InputManager.get_managed_gamepads()
-	for device in blacklist:
-		sandbox.append("--blacklist=%s" % device)
-	sandbox.append("--")
-	sandbox.append_array(args)
-	steam_process = InteractiveProcess.new("firejail", sandbox)
-	if steam_process.start() != OK:
-		logger.error("Failed to start steam process.")
-	if not "steam" in args:
-		return
-	var logger_func := func(delta: float):
-		steam_process.output_to_log_file(steam_log)
-	shared_thread.add_process(logger_func)
-	# Look for steam
-	while not steam_window_id:
-		
+
+	if "steam" in args:
+		_start_steam_process(args)
+	else:
+		var log_path := OS.get_environment("HOME") + "/.underlay-stdout.log"
+		_start_underlay_process(args, log_path)
+
+
+func _start_steam_process(args: Array) -> void:
+	# Setup steam
+	var underlay_log_path = OS.get_environment("HOME") + "/.steam-stdout.log"
+	_start_underlay_process(args, underlay_log_path)
+
+	# Look for steam and save window ID
+	while not underlay_window_id:
 		# Find Steam in the display tree
 		var root_win_id := Gamescope.get_root_window_id(display)
 		var all_windows := Gamescope.get_all_windows(root_win_id, display)
@@ -95,11 +72,13 @@ func _setup_qam_only(args: Array) -> void:
 			if window == qam_window_id:
 				continue
 			if Gamescope.has_xprop(window, "STEAM_OVERLAY", display):
-				steam_window_id = window
-				logger.debug("Found steam! " + str(steam_window_id))
+				underlay_window_id = window
+				logger.debug("Found steam! " + str(underlay_window_id))
 				break
 		# Wait a bit to reduce cpu load.
 		OS.delay_msec(1000)
+
+	# Start timer to check if steam has exited.
 	var exit_timer := Timer.new()
 	exit_timer.set_one_shot(false)
 	exit_timer.set_timer_process_callback(Timer.TIMER_PROCESS_IDLE)
@@ -108,22 +87,54 @@ func _setup_qam_only(args: Array) -> void:
 	exit_timer.start()
 
 
-## Called when "qam_state" is entered. Makes the QAM visible.
+func _start_underlay_process(args: Array, log_path: String) -> void:
+	# Start the shared thread we use for logging.
+	shared_thread = SharedThread.new()
+	shared_thread.start()
+
+	# Setup logging
+	underlay_log = FileAccess.open(log_path, FileAccess.WRITE)
+	var error := underlay_log.get_open_error()
+	if error != OK:
+		logger.warn("Got error opening log file.")
+	else:
+		logger.info("Started logging underlay process at " + log_path)
+	# Launch underlay in the sandbox
+	var sandbox := PackedStringArray()
+	sandbox.append_array(["--noprofile"])
+	var blacklist := InputManager.get_managed_gamepads()
+	for device in blacklist:
+		sandbox.append("--blacklist=%s" % device)
+	sandbox.append("--")
+	sandbox.append_array(args)
+	underlay_process = InteractiveProcess.new("firejail", sandbox)
+	if underlay_process.start() != OK:
+		logger.error("Failed to start child process.")
+		return
+	var logger_func := func(delta: float):
+		underlay_process.output_to_log_file(underlay_log)
+	shared_thread.add_process(logger_func)
+
+
+## Called when "qam_state" is entered.
 func _on_qam_open(_from: State) -> void:
 	InputManager._set_intercept(ManagedGamepad.INTERCEPT_MODE.ALL)
 	Gamescope.set_overlay(qam_window_id, 1, display)
-	Gamescope.set_overlay(steam_window_id, 0, display)
+	if underlay_window_id:
+		Gamescope.set_overlay(underlay_window_id, 0, display)
 
 
-## Called when "qam_state" is exited. Makes the QAM invisible.
+## Called when "qam_state" is exited.
 func _on_qam_closed(_to: State) -> void:
 	InputManager._set_intercept(ManagedGamepad.INTERCEPT_MODE.PASS_QAM)
 	Gamescope.set_overlay(qam_window_id, 0, display)
-	Gamescope.set_overlay(steam_window_id, 1, display)
+	if underlay_window_id:
+		Gamescope.set_overlay(underlay_window_id, 1, display)
+
 
 ## Verifies steam is still running by checking for the steam overlay, closes otherwise.
 func _check_exit() -> void:
-	if Gamescope.has_xprop(steam_window_id, "STEAM_OVERLAY", display):
+	if Gamescope.has_xprop(underlay_window_id, "STEAM_OVERLAY", display):
 		return
 	logger.debug("Steam closed. Shutting down.")
 	get_tree().quit()
