88ed0ebcb2795c73bf2b3808bf335537a6e40681
Derek J. Clark <derkejohn.clark@gmail.com>
2023-04-02 19:18:52 -0700

    feat(platform): Use platform for hardware detect. (#103)
    * Adds APUDatabase resource, replaces the dictionary in powertools.
    * Adds APUEntry resource. Outlines TDP settings for an APU by model.
    * Adds CPUInfo and GPUInfo data classes to store relevant capabilities of a given CPU/GPU
    * Powertools now loads data from Platform on the CPU and GPU
    * Add CPU and GPU model display to general settings menu.

---
+++ core/platform/hardware/apu_database.gd
@@ -0,0 +1,22 @@
+extends Resource
+
+class_name APUDatabase
+
+@export var apu_list: Array[APUEntry]
+@export var database_name: String
+var apu_map: Dictionary
+
+var logger := Log.get_logger(database_name+"APUDatabase", Log.LEVEL.INFO)
+
+
+func init()-> void:
+	logger.debug("Setting up APU database map.")
+	for apu in apu_list:
+		apu_map[apu.model_name] = apu
+
+
+func get_apu(apu_name: String) -> APUEntry:
+	if not apu_name in apu_map:
+		logger.info("APU " + apu_name + " not in APU Database")
+		return null
+	return apu_map[apu_name]	
