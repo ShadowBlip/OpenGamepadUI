5e5913f23eb9086a14359d54ce70832c001e931f
William Edwards <shadowapex@gmail.com>
2023-04-03 20:21:30 -0700

    fix(Platform): add shortcut to gnome dash and ensure icon exists

--- core/platform/chimeraos.gd
+++ core/platform/chimeraos.gd
@@ -2,8 +2,12 @@ extends PlatformProvider
 class_name PlatformChimeraOS
 
 var HOME := OS.get_environment("HOME")
-var DESKTOP_PATH := "/".join(["/home", HOME, "Desktop"])
-var DESKTOP_FILE_PATH := "/".join([DESKTOP_PATH, "return-opengamepadui.desktop"])
+var DESKTOP_PATH := "/".join([HOME, ".local", "share", "applications"])
+var DESKTOP_FILENAME := "return-opengamepadui.desktop"
+var DESKTOP_FILE_PATH := "/".join([DESKTOP_PATH, DESKTOP_FILENAME])
+var ICON_PATH := "/".join([HOME, ".local", "share", "icons", "hicolor", "scalable", "apps"])
+var ICON_FILENAME := "opengamepadui.svg"
+var ICON_FILE_PATH := "/".join([ICON_PATH, ICON_FILENAME])
 const DESKTOP_FILE := "#!/usr/bin/env xdg-open
 [Desktop Entry]
 Version=1.0
@@ -18,6 +22,7 @@ Terminal=false"
 
 func _init() -> void:
 	logger.set_name("PlatformChimeraOS")
+	_ensure_desktop_icon()
 	_ensure_desktop_file()
 
 
@@ -72,6 +77,7 @@ func _has_session_switcher() -> bool:
 
 ## Ensure there is a "Return to OpenGamepadUI" desktop shortcut
 func _ensure_desktop_file() -> void:
+	# Create the desktop entry
 	if FileAccess.file_exists(DESKTOP_FILE_PATH):
 		return
 	if not DirAccess.dir_exists_absolute(DESKTOP_PATH):
@@ -80,6 +86,60 @@ func _ensure_desktop_file() -> void:
 	desktop_file.store_string(DESKTOP_FILE)
 	desktop_file.close()
 	OS.execute("chmod", ["+x", DESKTOP_FILE_PATH])
+	
+	# Ensure that there's a dashboard entry
+	var favorites := get_gnome_favorites()
+	if DESKTOP_FILENAME in favorites:
+		return
+	favorites.append(DESKTOP_FILENAME)
+	set_gnome_favorites(favorites)
+
+
+## Ensure the OpenGamepadUI icon exists
+func _ensure_desktop_icon() -> void:
+	if FileAccess.file_exists(ICON_FILE_PATH):
+		return
+	if not DirAccess.dir_exists_absolute(ICON_PATH):
+		DirAccess.make_dir_recursive_absolute(ICON_PATH)
+	var bytes := FileAccess.get_file_as_bytes("res://icon.svg")
+	var icon_file := FileAccess.open(ICON_FILE_PATH, FileAccess.WRITE)
+	icon_file.store_buffer(bytes)
+	icon_file.flush()
+
+
+## Sets the gnome dashboard favorites to the given desktop items
+func set_gnome_favorites(favorites: PackedStringArray) -> int:
+	var apps_format := "[{0}]"
+	var apps_arr: Array[String] = []
+	for fav in favorites:
+		apps_arr.append("'" + fav + "'")
+	var apps := apps_format.format([", ".join(apps_arr)])
+	
+	var output := []
+	var code := OS.execute("gsettings", ["set", "org.gnome.shell", "favorite-apps", apps], output)
+	if code != OK:
+		logger.error("Unable to pin OpenGamepadUI shortcut to dashboard: " + output[0])
+	return code
+
+
+## Return all the pinned dashboard items
+## [gamer@chimeraos ~]$ gsettings get org.gnome.shell favorite-apps
+## ['org.gnome.Nautilus.desktop', 'org.gnome.Software.desktop']
+func get_gnome_favorites() -> PackedStringArray:
+	var output := []
+	if OS.execute("gsettings", ["get", "org.gnome.shell", "favorite-apps"], output) != OK:
+		return PackedStringArray()
+	var stdout := output[0] as String
+	stdout = stdout.replace("[", "")
+	stdout = stdout.replace("]", "")
+	stdout = stdout.replace("'", "")
+	stdout = stdout.replace("\"", "")
+	stdout = stdout.replace(" ", "")
+	stdout = stdout.strip_edges()
+	var favorites := stdout.split(",")
+	
+	return PackedStringArray(favorites)
+	
 
 
 ## Switch to the given session
