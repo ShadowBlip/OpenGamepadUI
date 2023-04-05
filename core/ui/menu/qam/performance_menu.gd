a9064cfbd4fe185405e301ed508c1a4d81800d67
Derek J. Clark <derekjohn.clark@gmail.com>
2023-04-04 18:23:27 -0700

    Remove command wait for performance and quick settings menu's

--- core/ui/menu/qam/performance_menu.gd
+++ core/ui/menu/qam/performance_menu.gd
@@ -1,7 +1,5 @@
 extends Control
 
-var command_timer: Timer
-
 var logger := Log.get_logger("PerformanceMenu", Log.LEVEL.INFO)
 
 @onready var mangoapp_slider := $%MangoAppSlider
@@ -9,21 +7,11 @@ var logger := Log.get_logger("PerformanceMenu", Log.LEVEL.INFO)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	command_timer = Timer.new()
-	command_timer.set_autostart(false)
-	command_timer.set_one_shot(true)
-	add_child(command_timer)
-
 	mangoapp_slider.value_changed.connect(_on_mangoapp_changed)
 
 
 # Set the mangoapp config on slider change
 func _on_mangoapp_changed(value: float) -> void:
-	_setup_callback_func(_do_mangoapp_change)
-
-
-func _do_mangoapp_change() -> void:
-	var value = mangoapp_slider.value
 	if value == 0:
 		MangoApp.set_config(MangoApp.CONFIG_NONE)
 		return
@@ -39,19 +27,3 @@ func _do_mangoapp_change() -> void:
 	if value >= 4:
 		MangoApp.set_config(MangoApp.CONFIG_INSANE)
 		return
-
-
-# Overrides or sets the command_timer.timeout signal connection function and
-# (re)starts the timer.
-func _setup_callback_func(callable: Callable) -> void:
-	logger.debug("Setting callback func")
-	_clear_callbacks()
-	command_timer.timeout.connect(callable, CONNECT_ONE_SHOT)
-	command_timer.start(.5)
-
-
-# Removes any existing signal connections to command_timer.timeout.
-func _clear_callbacks() -> void:
-	for connection in command_timer.timeout.get_connections():
-		var callable := connection["callable"] as Callable
-		command_timer.timeout.disconnect(callable)
