4ad5b68093629256fee7b2a0d1c56748794661be
Derek J. Clark <derekjohn.clark@gmail.com>
2023-04-04 17:01:50 -0700

    fix(QAM): General QAM improvements

--- core/ui/menu/qam/performance_menu.gd
+++ core/ui/menu/qam/performance_menu.gd
@@ -1,15 +1,32 @@
 extends Control
 
+var command_timer: Timer
+var shared_thread: SharedThread
+
+var logger := Log.get_logger("PerformanceMenu", Log.LEVEL.INFO)
+
 @onready var mangoapp_slider := $%MangoAppSlider
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	shared_thread = SharedThread.new()
+	shared_thread.start()
+	command_timer = Timer.new()
+	command_timer.set_autostart(false)
+	command_timer.set_one_shot(true)
+	add_child(command_timer)
+
 	mangoapp_slider.value_changed.connect(_on_mangoapp_changed)
 
 
 # Set the mangoapp config on slider change
 func _on_mangoapp_changed(value: float) -> void:
+	_setup_callback_func(_do_mangoapp_change)
+
+
+func _do_mangoapp_change() -> void:
+	var value = mangoapp_slider.value
 	if value == 0:
 		MangoApp.set_config(MangoApp.CONFIG_NONE)
 		return
@@ -25,3 +42,19 @@ func _on_mangoapp_changed(value: float) -> void:
 	if value >= 4:
 		MangoApp.set_config(MangoApp.CONFIG_INSANE)
 		return
+
+
+# Overrides or sets the command_timer.timeout signal connection function and
+# (re)starts the timer.
+func _setup_callback_func(callable: Callable) -> void:
+	logger.debug("Setting callback func")
+	_clear_callbacks()
+	command_timer.timeout.connect(callable, CONNECT_ONE_SHOT)
+	command_timer.start(.5)
+
+
+# Removes any existing signal connections to command_timer.timeout.
+func _clear_callbacks() -> void:
+	for connection in command_timer.timeout.get_connections():
+		var callable := connection["callable"] as Callable
+		command_timer.timeout.disconnect(callable)
--- core/ui/menu/qam/quick_settings_menu.gd
+++ core/ui/menu/qam/quick_settings_menu.gd
@@ -5,7 +5,10 @@ var AudioManager := preload("res://core/global/audio_manager.tres") as AudioMana
 var DisplayManager := preload("res://core/global/display_manager.tres") as DisplayManager
 
 var backlights := DisplayManager.get_backlight_paths()
-var logger := Log.get_logger("QuickSettings")
+var command_timer: Timer
+var shared_thread: SharedThread
+
+var logger := Log.get_logger("QuickSettings", Log.LEVEL.INFO)
 
 @onready var output_volume := $%VolumeSlider
 @onready var brightness_slider := $%BrightnessSlider
@@ -14,6 +17,13 @@ var logger := Log.get_logger("QuickSettings")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	shared_thread = SharedThread.new()
+	shared_thread.start()
+	command_timer = Timer.new()
+	command_timer.set_autostart(false)
+	command_timer.set_one_shot(true)
+	add_child(command_timer)
+
 	var current_volume := AudioManager.get_current_volume()
 	output_volume.value = current_volume * 100
 	output_volume.value_changed.connect(_on_output_volume_slider_changed)
@@ -36,16 +46,44 @@ func _on_output_volume_changed(value: float) -> void:
 
 
 func _on_output_volume_slider_changed(value: float) -> void:
-	var percent := value * 0.01
+	_setup_callback_func(_do_output_volume_slider_change)
+
+
+func _do_output_volume_slider_change() -> void:
+	var percent: float = output_volume.value * 0.01
 	AudioManager.set_volume(percent)
 
 
-func _on_brightness_slider_changed(value: float) -> void:
-	var percent := value * 0.01
+func _on_brightness_slider_changed(_value: float) -> void:
+	_setup_callback_func(_do_brightness_slider_change)
+
+
+func _do_brightness_slider_change() -> void:
+	var percent: float = brightness_slider.value * 0.01
 	DisplayManager.set_brightness(percent)
 
 
-func _on_saturation_changed(value: float) -> void:
-	var code := Gamescope.set_saturation(value / 100.0)
+func _on_saturation_changed(_value: float) -> void:
+	_setup_callback_func(_do_brightness_slider_change)
+
+
+func _do_saturation_change() -> void:
+	var code := Gamescope.set_saturation(saturation_slider.value / 100.0)
 	if code != OK:
 		logger.warn("Unable to set saturation. Code: " + str(code))
+
+
+# Overrides or sets the command_timer.timeout signal connection function and
+# (re)starts the timer.
+func _setup_callback_func(callable: Callable) -> void:
+	logger.debug("Setting callback func")
+	_clear_callbacks()
+	command_timer.timeout.connect(callable, CONNECT_ONE_SHOT)
+	command_timer.start(.5)
+
+
+# Removes any existing signal connections to command_timer.timeout.
+func _clear_callbacks() -> void:
+	for connection in command_timer.timeout.get_connections():
+		var callable := connection["callable"] as Callable
+		command_timer.timeout.disconnect(callable)
