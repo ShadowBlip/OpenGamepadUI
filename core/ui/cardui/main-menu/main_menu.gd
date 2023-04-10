d99fba95e723aebb41caf84f317b8fd089a89fd1
William Edwards <shadowapex@gmail.com>
2023-04-10 15:26:15 -0700

    Add running game card

--- core/ui/cardui/main-menu/main_menu.gd
+++ core/ui/cardui/main-menu/main_menu.gd
@@ -1,6 +1,44 @@
 extends Control
 
+const RunningGameCard := preload("res://core/ui/cardui/navigation/running_game_card.gd")
+const running_card_scene := preload("res://core/ui/cardui/navigation/running_game_card.tscn")
+
+var launch_manager := load("res://core/global/launch_manager.tres") as LaunchManager
+var state_machine := preload("res://assets/state/state_machines/global_state_machine.tres") as StateMachine
+var main_menu_state := preload("res://assets/state/states/main_menu.tres") as State
+var in_game_menu_state := preload("res://assets/state/states/in_game_menu.tres") as State
+
+@onready var focus_node := $%LibraryButton
+@onready var button_container := $%ButtonContainer
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	main_menu_state.state_entered.connect(_on_state_entered)
+	in_game_menu_state.state_entered.connect(_on_state_entered)
+	launch_manager.app_launched.connect(_on_app_launched)
+	
+	
+func _on_state_entered(_from: State) -> void:
+	if state_machine.current_state() == main_menu_state:
+		focus_node.grab_focus.call_deferred()
+
+
+func _on_app_launched(app: RunningApp):
+	# Create a new running game card
+	var card: RunningGameCard = running_card_scene.instantiate()
+	card.set_running_app(app)
+
+	# Switch to the app if its card is pressed
+	var on_pressed := func():
+		launch_manager.set_current_app(app)
+	card.pressed.connect(on_pressed)
+
+	# Remove the card when it is killed
+	var on_app_killed := func():
+		card.queue_free()
+	app.app_killed.connect(on_app_killed)
+
+	# Add and move the card to the menu
+	button_container.add_child(card)
+	button_container.move_child(card, 0)
