c076c770489d3579d072c105a648eb4b7a28f083
William Edwards <shadowapex@gmail.com>
2023-05-11 21:30:16 -0700

    feat(OOTBE): Add out-of-the-box experience menus on first boot

--- core/ui/card_ui/ootbe/first_boot_menu.gd
+++ core/ui/card_ui/ootbe/first_boot_menu.gd
@@ -1,7 +1,19 @@
 extends Control
 
+const SettingsManager := preload("res://core/global/settings_manager.tres")
+
+@onready var finished_menu := $%FinishMenu
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if not SettingsManager.get_value("general", "first_boot", true):
+		finish()
+		return
+	finished_menu.finished.connect(finish)
 
+
+## Removes the menu from the scene when all first-boot menus are done or user
+## has already ran first boot steps.
+func finish() -> void:
+	queue_free()
--- core/ui/card_ui/ootbe/network_select.gd
+++ core/ui/card_ui/ootbe/network_select.gd
@@ -1,6 +1,19 @@
 extends MarginContainer
 
+var state_machine := load("res://assets/state/state_machines/first_boot_state_machine.tres") as StateMachine
+var next_state := load("res://assets/state/states/first_boot_plugin_select.tres") as State
+var network_state := load("res://assets/state/states/first_boot_network.tres") as State
 
-# Called when the node enters the scene tree for the first time.
+var logger := Log.get_logger("NetworkSelect")
+
+
+# Called when the node enters the scene tree for the first time.`
 func _ready() -> void:
-	pass # Replace with function body.
+	network_state.state_entered.connect(_on_state_entered)
+
+
+func _on_state_entered(_from: State) -> void:
+	if NetworkManager.supports_network():
+		return
+	logger.info("Networking is not supported. Skipping.")
+	state_machine.replace_state(next_state)
