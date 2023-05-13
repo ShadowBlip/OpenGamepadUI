8e352e115997d0e6a9cedbedbe6ef12fbf5580c4
Derek J. Clark <derkejohn.clark@gmail.com>
2023-05-13 11:32:28 -0700

    fix(InputManager): Use the correct InputManager in onlyQAM mode. (#149)

--- core/systems/input/only_qam_input_manager.gd
+++ core/systems/input/only_qam_input_manager.gd
@@ -9,6 +9,9 @@ var process_input_during: Array[State] = []
 
 var handle_back: bool = false
 
+## Will show logger events with the prefix OnlyQAMInputManager
+var logger := Log.get_logger("OnlyQAMInputManager", Log.LEVEL.INFO)
+
 func _ready() -> void:
 	InputManager.init()
 	state_machine.state_changed.connect(_on_state_changed)
@@ -18,8 +21,10 @@ func _ready() -> void:
 # Only process input when the given states are active
 func _on_state_changed(_from: State, to: State) -> void:
 	if to in process_input_during:
+		logger.debug("Handle Back")
 		handle_back = true
 		return
+	logger.debug("Don't handle back.")
 	handle_back = false
 
 
@@ -27,15 +32,15 @@ func _input(event: InputEvent) -> void:
 	var valid_events: Array = [event.is_action_pressed("ogui_qam"), event.is_action_pressed("ogui_east")]
 	if not true in valid_events:
 		return
-	
+
 	if event.is_action_pressed("ogui_qam"):
 		InputManager._qam_input(event)
 		get_viewport().set_input_as_handled()
 		return
-	
+
 	if not handle_back:
 		return
-
+	logger.debug("Pop state")
 	# Pop the state machine stack to go back
 	state_machine.pop_state()
 	get_viewport().set_input_as_handled()
