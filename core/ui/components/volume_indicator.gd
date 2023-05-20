dbf51f9d508d305f41a218f7a7901e8cbd61de7b
William Edwards <shadowapex@gmail.com>
2023-05-20 11:34:28 -0700

    fix(LaunchManager): better handle setting the overlay property and initial focus

--- core/ui/components/volume_indicator.gd
+++ core/ui/components/volume_indicator.gd
@@ -1,10 +1,6 @@
 extends Control
 
-var Gamescope := load("res://core/global/gamescope.tres") as Gamescope
 var AudioManager := load("res://core/global/audio_manager.tres") as AudioManager
-var state_machine := load("res://assets/state/state_machines/global_state_machine.tres") as StateMachine
-var in_game_state := load("res://assets/state/states/in_game.tres") as State
-var PID: int = OS.get_process_id()
 
 @onready var timer := $%Timer as Timer
 @onready var level_indicator := $%LevelIndicator
@@ -18,12 +14,6 @@ func _ready() -> void:
 
 
 func _on_volume_changed(value: float) -> void:
-	# Check to see if OGUI is set as an overlay
-	var ogui_window_id = Gamescope.get_window_id(PID, Gamescope.XWAYLAND.OGUI)
-	var is_overlay := Gamescope.get_overlay(ogui_window_id)
-	if is_overlay == 0:
-		Gamescope.set_overlay(ogui_window_id, 1)
-		
 	visible = true
 	level_indicator.value = value * 100
 	timer.start()
@@ -31,6 +21,3 @@ func _on_volume_changed(value: float) -> void:
 
 func _on_timeout() -> void:
 	visible = false
-	if state_machine.current_state() == in_game_state:
-		var ogui_window_id = Gamescope.get_window_id(PID, Gamescope.XWAYLAND.OGUI)
-		Gamescope.set_overlay(ogui_window_id, 0)
