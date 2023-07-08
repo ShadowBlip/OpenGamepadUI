037c82b4a3c489a9f223e670a543ae13c93652b0
Derek J. Clark <derekjohn.clark@gmail.com>
2023-07-08 11:23:34 -0700

    fix(platform): Fix A1 Pro integration

--- core/platform/aokzoe_gen2.gd
+++
@@ -1,7 +0,0 @@
-extends PlatformProvider
-class_name PlatformAOKZOEGen2
-
-
-func get_handheld_gamepad() -> HandheldGamepad:
-	logger.info("Using AOKZOE Gen2 platform configuration.")
-	return load("res://core/platform/aokzoe_gen2_gamepad.tres")
