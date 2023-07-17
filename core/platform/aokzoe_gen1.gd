794a5697938afac7d0dfbbfdf66dcfbf37c196c3
Derek J. Clark <derekjohn.clark@gmail.com>
2023-07-17 15:18:09 -0700

    fix(Platform): Add Turbo Takeover to AOKZOE line.

--- core/platform/aokzoe_gen1.gd
+++ core/platform/aokzoe_gen1.gd
@@ -4,4 +4,9 @@ class_name PlatformAOKZOEGen1
 
 func get_handheld_gamepad() -> HandheldGamepad:
 	logger.info("Using AOKZOE Gen 1 platform configuration.")
+	var cmd := Command.new("/usr/share/opengamepadui/scripts/manage_input", ["turbo_takeover", "1"])
+	var code := await cmd.execute() as int
+	if code != OK:
+		logger.warn("Unable to take over turbo button: " + cmd.stdout)
+
 	return load("res://core/platform/aokzoe_gen1_gamepad.tres")
