8bf94a01635d85ab63c1d599d6aa15b4e12da316
William Edwards <shadowapex@gmail.com>
2023-07-16 19:31:39 -0700

    fix(OXP): Enable turbo takeover for OXP Mini A07

--- core/platform/onexplayer_gen4.gd
+++ core/platform/onexplayer_gen4.gd
@@ -1,12 +1,12 @@
 extends PlatformProvider
-class_name PlatformOneXPlayerGen3
+class_name PlatformOneXPlayerGen4
 
 
 func get_handheld_gamepad() -> HandheldGamepad:
-	logger.info("Using OneXPlayer Gen 3 platform configuration.")
+	logger.info("Using OneXPlayer Gen 4 platform configuration.")
 	var cmd := Command.new("/usr/share/opengamepadui/scripts/manage_input", ["turbo_takeover", "1"])
 	var code := await cmd.execute() as int
 	if code != OK:
 		logger.warn("Unable to take over turbo button: " + cmd.stdout)
-	
-	return load("res://core/platform/onexplayer_gen3_gamepad.tres")
+
+	return load("res://core/platform/onexplayer_gen4_gamepad.tres")
