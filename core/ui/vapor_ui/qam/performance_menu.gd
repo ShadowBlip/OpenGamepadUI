131e301554f0dd4dfb98b7040d9b97fe2938139e
William Edwards <shadowapex@gmail.com>
2023-08-02 09:29:24 -0700

    feat(QAM): add FPS limit slider

--- core/ui/vapor_ui/qam/performance_menu.gd
+++ core/ui/vapor_ui/qam/performance_menu.gd
@@ -1,13 +1,18 @@
 extends Control
 
+var gamescope := load("res://core/global/gamescope.tres") as Gamescope
 var logger := Log.get_logger("PerformanceMenu", Log.LEVEL.INFO)
 
 @onready var mangoapp_slider := $%MangoAppSlider
+@onready var fps_slider := $%FramerateLimitSlider
+@onready var fps_timer := $%FramerateChangeTimer as Timer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	mangoapp_slider.value_changed.connect(_on_mangoapp_changed)
+	fps_slider.value_changed.connect(_on_fps_limit_changed)
+	fps_timer.timeout.connect(_on_fps_limit_timer_timeout)
 
 
 # Set the mangoapp config on slider change
@@ -27,3 +32,14 @@ func _on_mangoapp_changed(value: float) -> void:
 	if value >= 4:
 		MangoApp.set_config(MangoApp.CONFIG_INSANE)
 		return
+
+
+# Whenever the FPS slider is changed, start a small timer to limit the number
+# of changes
+func _on_fps_limit_changed(_value: float) -> void:
+	fps_timer.start()
+
+
+# Update the gamescope FPS limit
+func _on_fps_limit_timer_timeout() -> void:
+	gamescope.set_fps_limit(fps_slider.value)
