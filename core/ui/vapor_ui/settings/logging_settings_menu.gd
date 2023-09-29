1c9883c6fb5a99b5c262edc233acba9df25d26cd
William Edwards <shadowapex@gmail.com>
2023-09-29 14:16:55 -0700

    fix(Logging): merge logging classes to avoid odd circular dependency errors

--- core/ui/vapor_ui/settings/logging_settings_menu.gd
+++ core/ui/vapor_ui/settings/logging_settings_menu.gd
@@ -3,7 +3,7 @@ extends ScrollContainer
 const toggle_scene := preload("res://core/ui/components/toggle.tscn")
 
 var logger_map := {}
-var log_manager := preload("res://core/global/log_manager.tres")
+var log_manager := preload("res://core/systems/debug/log_manager.tres") as Log
 
 @onready var container := $%VBoxContainer
 @onready var focus_group := $%FocusGroup
