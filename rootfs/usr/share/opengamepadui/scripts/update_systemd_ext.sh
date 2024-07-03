055a80a75c57c0946be3943e07affcdc5f2741cb
William Edwards <shadowapex@gmail.com>
2024-07-02 21:59:14 -0700

    chore(SystemdExt): don't remove archive until squashfs successfully runs

--- rootfs/usr/share/opengamepadui/scripts/update_systemd_ext.sh
+++ rootfs/usr/share/opengamepadui/scripts/update_systemd_ext.sh
@@ -6,5 +6,6 @@ rm -rf squashfs-root
 unsquashfs -no-xattrs opengamepadui.raw
 grep '^ID=' /etc/os-release >squashfs-root/usr/lib/extension-release.d/extension-release.opengamepadui
 grep '^VERSION_ID=' /etc/os-release >>squashfs-root/usr/lib/extension-release.d/extension-release.opengamepadui
-rm -f opengamepadui.raw
-mksquashfs squashfs-root opengamepadui.raw
+mksquashfs squashfs-root opengamepadui.raw.updated
+mv opengamepadui.raw.updated opengamepadui.raw
+rm -rf squashfs-root
